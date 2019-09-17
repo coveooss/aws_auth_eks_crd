491e0238b9e63ac4798db9eacf372b493f087a97
jheil <jheil@coveo.com>
2019-09-17 10:49:08 -0400

    Add full_sync on start

--- iam_mapping.py
+++ iam_mapping.py
@@ -13,29 +13,22 @@ except Exception as e:
     logger.info("Could not load kubeconfig. Error is : {}".format(e))
 
 API = client.CoreV1Api()
-
-
-@kopf.on.create('iamauthenticator.k8s.aws', 'v1alpha1', 'iamidentitymappings')
-def create_mapping(body: dict, meta: dict, spec: dict, **kwargs):
-    logger.info('Adding mapping for user {} as {} to {}'.format(
-        spec['userarn'], spec['username'], spec['groups']))
-
-    cm = API.read_namespaced_config_map('aws-auth', 'kube-system')
-    users = get_user_mapping(cm)
-    updated_mapping = ensure_user(spec, users)
-    apply_mapping(cm, updated_mapping)
-
+custom_objets_API = client.CustomObjectsApi()
 
 @kopf.on.update('iamauthenticator.k8s.aws', 'v1alpha1', 'iamidentitymappings')
-def update_mapping(body: dict, meta: dict, spec: dict, **kwargs):
-    logger.info('Update mapping for user {} as {} to {}'.format(
-        spec['userarn'], spec['username'], spec['groups']))
+@kopf.on.create('iamauthenticator.k8s.aws', 'v1alpha1', 'iamidentitymappings')
+async def create_mapping(body: dict, meta: dict, spec: dict, event: str, diff: set, **kwargs):
+    # Do nothing when we have no diff
+    if len(diff) < 1:
+        return dict()
+
+    logger.info('{} mapping for user {} as {} to {}'.format(
+        event.title(), spec['userarn'], spec['username'], spec['groups']))
 
     cm = API.read_namespaced_config_map('aws-auth', 'kube-system')
     users = get_user_mapping(cm)
     updated_mapping = ensure_user(spec, users)
     apply_mapping(cm, updated_mapping)
-
 
 @kopf.on.delete('iamauthenticator.k8s.aws', 'v1alpha1', 'iamidentitymappings')
 def delete_mapping(body: dict, meta: dict, spec: dict, **kwargs):
@@ -47,18 +40,27 @@ def delete_mapping(body: dict, meta: dict, spec: dict, **kwargs):
     updated_mapping = delete_user(spec, users)
     apply_mapping(cm, updated_mapping)
 
+def full_synchronize():
+    # Get Kubernetes' objects
+    cm = API.read_namespaced_config_map('aws-auth', 'kube-system')
+    identity_mappings = custom_objets_API.list_cluster_custom_object('iamauthenticator.k8s.aws', 'v1alpha1', 'iamidentitymappings')
+
+    users = get_user_mapping(cm)
+    users = users if type(users) == "list" else list()
+    for im in identity_mappings["items"]:
+        users = ensure_user(im["spec"], users)
+    apply_mapping(cm, users)
 
 def get_user_mapping(cm):
-    if 'mapUsers' in cm.data:
+    try:
         return yaml.safe_load(cm.data['mapUsers'])
-    else:
+    except:
         return []
 
 
 def apply_mapping(existing_cm, user_mapping):
-    existing_cm.data["mapUsers"] = yaml.dump(user_mapping)
+    existing_cm.data["mapUsers"] = yaml.safe_dump(user_mapping)
     API.patch_namespaced_config_map('aws-auth', 'kube-system', existing_cm)
-
 
 def ensure_user(user, user_list):
     for i, existing_user in enumerate(user_list):
@@ -81,3 +83,6 @@ def delete_user(user, user_list):
         raise Exception(
             "Want to delete {}, but not found".format(user['username']))
     return user_list
+
+# Do a full synchronization at the start
+full_synchronize()
