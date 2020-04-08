560e51f4c39de809733e7ee58dbb53258dbf82f6
Thomas <tdegiacinto@coveo.com>
2020-04-08 14:39:34 +0200

    Add liveness probe

--- iam_mapping.py
+++ iam_mapping.py
@@ -4,13 +4,16 @@ import copy
 import logging
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
+from kubernetes.client.models.v1_config_map import V1ConfigMap
 
 logger = logging.getLogger('operator')
 
 try:
-    config.load_kube_config('./kubeconfig')
+    # config.load_kube_config('./kubeconfig')
+    config.load_kube_config()
 except Exception as e:
-    logger.info("Could not load kubeconfig. Error is : {}".format(e))
+    logger.info(
+        "Could not load kubeconfig. Error is : {}.\nAssuming we are in a kubernetes cluster".format(e))
 
 API = client.CoreV1Api()
 custom_objects_API = client.CustomObjectsApi()
@@ -21,7 +24,7 @@ PLURAL = 'iamidentitymappings'
 
 @kopf.on.update(GROUP, VERSION, PLURAL)
 @kopf.on.create(GROUP, VERSION, PLURAL)
-async def create_mapping(body: dict, meta: dict, spec: dict, event: str, diff: set, **kwargs):
+async def create_mapping(body: dict, meta: dict, spec: dict, event: str, diff: set, **kwargs) -> None:
     # Do nothing when we have no diff
     if len(diff) < 1:
         return dict()
@@ -36,7 +39,7 @@ async def create_mapping(body: dict, meta: dict, spec: dict, event: str, diff: s
 
 
 @kopf.on.delete(GROUP, VERSION, PLURAL)
-def delete_mapping(body: dict, meta: dict, spec: dict, **kwargs):
+def delete_mapping(body: dict, meta: dict, spec: dict, **kwargs) -> None:
     logger.info('Delete mapping for user {} as {} to {}'.format(
         spec['userarn'], spec['username'], spec['groups']))
 
@@ -46,7 +49,32 @@ def delete_mapping(body: dict, meta: dict, spec: dict, **kwargs):
     apply_mapping(cm, updated_mapping)
 
 
-def deploy_crd_definition():
+def check_synchronization() -> str:
+    cm = API.read_namespaced_config_map('aws-auth', 'kube-system')
+    identity_mappings = custom_objects_API.list_cluster_custom_object(
+        GROUP, VERSION, PLURAL)
+
+    users_in_cm = get_user_mapping(cm)
+    users_in_cm = users_in_cm if isinstance(users_in_cm, list) else list()
+    users_in_cm = [u['username'] for u in users_in_cm]
+
+    users_in_crd = [im["spec"]['username']
+                    for im in identity_mappings["items"]]
+
+    if (set(users_in_cm) == set(users_in_crd)):
+        raise Exception("monitoring check result : out-of-sync")
+        return "in-sync"
+    else:
+        # Raise exception to make the monitoring probe fail
+        raise Exception("monitoring check result : out-of-sync")
+
+
+@kopf.on.probe(id='sync')
+def get_monitoring_status(**kwargs) -> str:
+    return check_synchronization()
+
+
+def deploy_crd_definition() -> None:
     with open("kubernetes/iamidentitymappings.yaml", 'r') as stream:
         body = yaml.safe_load(stream)
     extensions_api = client.ApiextensionsV1beta1Api()
@@ -60,12 +88,20 @@ def deploy_crd_definition():
                 raise err
 
 
-def full_synchronize():
+@kopf.on.startup()
+def on_startup(logger: logger, **kwargs) -> None:
+    # Do a full synchronization at the start
+    logger.info("Deploy CRD definition")
+    deploy_crd_definition()
+    logger.info("Reconcile all existing ressources")
+    full_synchronize()
+
+
+def full_synchronize() -> None:
     # Get Kubernetes' objects
     cm = API.read_namespaced_config_map('aws-auth', 'kube-system')
     identity_mappings = custom_objects_API.list_cluster_custom_object(
         GROUP, VERSION, PLURAL)
-
     users = get_user_mapping(cm)
     users = users if type(users) == "list" else list()
     for im in identity_mappings["items"]:
@@ -73,19 +109,19 @@ def full_synchronize():
     apply_mapping(cm, users)
 
 
-def get_user_mapping(cm):
+def get_user_mapping(cm: V1ConfigMap) -> list:
     try:
         return yaml.safe_load(cm.data['mapUsers'])
     except:
         return []
 
 
-def apply_mapping(existing_cm, user_mapping):
+def apply_mapping(existing_cm: V1ConfigMap, user_mapping: dict) -> None:
     existing_cm.data["mapUsers"] = yaml.safe_dump(user_mapping)
     API.patch_namespaced_config_map('aws-auth', 'kube-system', existing_cm)
 
 
-def ensure_user(user, user_list):
+def ensure_user(user: dict, user_list: list) -> list:
     for i, existing_user in enumerate(user_list):
         # Handle existing user
         if existing_user['username'] == user['username']:
@@ -97,7 +133,7 @@ def ensure_user(user, user_list):
     return user_list
 
 
-def delete_user(user, user_list):
+def delete_user(user: dict, user_list: list) -> list:
     for i, existing_user in enumerate(user_list):
         if existing_user['username'] == user['username']:
             del user_list[i]
@@ -106,8 +142,3 @@ def delete_user(user, user_list):
         raise Exception(
             "Want to delete {}, but not found".format(user['username']))
     return user_list
-
-
-# Do a full synchronization at the start
-deploy_crd_definition()
-full_synchronize()
